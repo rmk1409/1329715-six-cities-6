60ff30b8abd69fe921f6977dcdbe8254e33bbb54
Roman Pogorelov <rmk140987@gmail.com>
2021-03-17 13:45:12 +0300

    2.1. Added mocks.

---
+++ src/mocks/offers.js
@@ -0,0 +1,40 @@
+const offers = [
+  {
+    "id": 1,
+    "preview_image": `img/1.png`,
+    "is_premium": false,
+    "price": 120,
+    "is_favorite": false,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `apartment`,
+    "rating": 4.8,
+
+    "images": [`img/1.png`, `img/2.png`],
+    "bedrooms": 3,
+    "max_adults": 1,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatar_url": `img/1.png`,
+      "id": 1,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Amsterdam`
+    },
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+  }
+];
+
+export {offers};
